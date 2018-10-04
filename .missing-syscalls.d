d136ed60dd13b7191b5833b1f321050ca992e7d9
Rafaël Carré <funman@videolan.org>
2018-10-04 10:25:23 +0200

    Delete ignored files (keep .config)

--- .missing-syscalls.d
+++
@@ -1,7 +0,0 @@
--:  ./include/generated/autoconf.h \
-  /media/HDD01/ralink/GPL/WAP300N/test1/WAP300N_GPL/RT288x_SDK/source/linux-2.6.36MT.x/arch/mips/include/asm/types.h \
-  include/asm-generic/int-ll64.h \
-  /media/HDD01/ralink/GPL/WAP300N/test1/WAP300N_GPL/RT288x_SDK/source/linux-2.6.36MT.x/arch/mips/include/asm/bitsperlong.h \
-  include/asm-generic/bitsperlong.h \
-  /media/HDD01/ralink/GPL/WAP300N/test1/WAP300N_GPL/RT288x_SDK/source/linux-2.6.36MT.x/arch/mips/include/asm/unistd.h \
-  /media/HDD01/ralink/GPL/WAP300N/test1/WAP300N_GPL/RT288x_SDK/source/linux-2.6.36MT.x/arch/mips/include/asm/sgidefs.h
